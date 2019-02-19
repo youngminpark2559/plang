5725818ac58a84d6ffb99800b815feeb89668a59
YoungMin Park <youngminpark2559@gmail.com>
2019-02-19 17:42:45 +0900

    Add files via upload

---
+++ cpp/yk/055_Dynamical_casting_upcasting_downcasting/main.cpp
@@ -0,0 +1,13 @@
+
+
+// ======================================================================
+// Dynamical casting
+
+// Dynamical means that you do when you need to do in a real time fashion
+
+// Convert an instance type of a sub class into an instance type of a base class
+// This is called "upcasting"
+// Upcasting doesn't lead to a loss of data
+
+// Converting an instance type of a base class into an instance type of a sub class is "downcasting"
+// Downcasting can lead to a loss of data
