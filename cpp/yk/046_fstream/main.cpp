57bc9f3fa9735dd085220a671501a4e37c6b647e
YoungMin Park <youngminpark2559@gmail.com>
2019-02-19 10:02:54 +0900

    Add files via upload

---
+++ cpp/yk/046_fstream/main.cpp
@@ -0,0 +1,71 @@
+
+
+// // ======================================================================
+// #include <iostream>
+// #include <cstring>
+// #include <fstream>
+// using namespace std;
+ 
+// int main()
+// {
+//   // c output: create a ofstream instance
+//   ofstream output;
+  
+//   // c open the file stream
+//   output.open("test.txt");
+
+//   // Put a text into the file
+//   output<<"Fun programming"<<endl;
+
+//   // c close the file stream
+//   output.close();
+
+//   return 0;
+// }
+
+// // ======================================================================
+// // Get the input text from a user
+// #include <iostream>
+// #include <cstring>
+// using namespace std;
+ 
+// int main()
+// {
+//   // c message: create char type array, size is 80
+//   char message[80];
+
+//   // Get a text from a user
+//   // then store the obtained text into the message array
+//   std::cin>>message;
+
+//   std::cout<<"message "<<message<<std::endl;
+
+//   return 0;
+// }
+
+// // ======================================================================
+// // Open a file
+// #include <iostream>
+// #include <fstream>
+// using namespace std;
+
+// int main()
+// {
+//   // c input: create the ifstream instance
+//   ifstream input;
+
+//   // c message: create char type array, size is 80
+//   char message[80];
+
+//   // c create a input file stream
+//   input.open("test.txt");
+
+//   // Put a text into a file
+//   input>>message;
+
+//   std::cout<<"message "<<message<<std::endl;
+
+//   input.close();
+// }
+
+// // ======================================================================
